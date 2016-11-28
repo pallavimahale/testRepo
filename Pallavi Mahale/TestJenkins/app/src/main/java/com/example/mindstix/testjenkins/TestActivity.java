7faf41d88abbb757ea1c883ed676f60a6a995984
Pallavi Mahale <pallavi.mahale@mindstix.com>
2016-11-28 18:15:18 +0530

    testing webhook

---
+++ Pallavi Mahale/TestJenkins/app/src/main/java/com/example/mindstix/testjenkins/TestActivity.java
@@ -0,0 +1,10 @@
+package com.example.mindstix.testjenkins;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mindstix on 11/28/2016.
+ */
+
+public class TestActivity extends AppCompatActivity {
+}
