e9887ca6f83bb00d4fa5b03e74f182be47695c59
Pallavi Mahale <pallavi.mahale@mindstix.com>
2016-11-29 18:00:30 +0530

    Changed to AppCompactActivity

--- Pallavi Mahale/TestJenkins/app/src/main/java/com/example/mindstix/testjenkins/MainActivity.java
+++ Pallavi Mahale/TestJenkins/app/src/main/java/com/example/mindstix/testjenkins/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.mindstix.testjenkins;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
