38962bfa4d4dcb87b898594effa7db79d625bf9e
Pallavi Mahale <pallavi.mahale@mindstix.com>
2016-11-29 15:46:09 +0530

    Appcompact replaced by activity

--- Pallavi Mahale/TestJenkins/app/src/main/java/com/example/mindstix/testjenkins/TestActivity.java
+++ Pallavi Mahale/TestJenkins/app/src/main/java/com/example/mindstix/testjenkins/TestActivity.java
@@ -6,5 +6,5 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Mindstix on 11/28/2016.
  */
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends Activity {
 }
